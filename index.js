49f6a76e84da80b9b7bba88ab409c1da803a8615
Sam Grundman <samuel@yodas.ws>
2018-05-05 13:26:15 -0400

    Linting

--- index.js
+++ index.js
@@ -7,84 +7,87 @@
  * MIT license.
  */
 
-'use strict';
+'use strict'
 
-var through = require('through2')
-  , rs = require('replacestream')
-  , gutil = require('gulp-util')
-  , extend = require('extend');
+const through = require('through2'),
+	rs = require('replacestream'),
+	gutil = require('gulp-util'),
+	extend = require('extend')
 
-var defaultOptions = {
-  logs: {
-    enabled: true,
-    notReplaced: false
-  }
-};
+const defaultOptions = {
+	logs: {
+		enabled: true,
+		notReplaced: false
+	}
+}
 
 module.exports = function (obj, replaceTo=null, userOptions=null) {
-	let replaceFrom = '';
-	let options = {};
+	let replaceFrom = ''
+	let options = {}
 
 	if (typeof obj === 'object' && ! (obj instanceof RegExp)) {
-		if (obj.pattern) replaceFrom = obj.pattern;
-		if (obj.replacement) replaceTo = obj.replacement;
-		options = extend(true, {}, obj, userOptions);
+		if (obj.pattern) replaceFrom = obj.pattern
+		if (obj.replacement) replaceTo = obj.replacement
+		options = extend(true, {}, obj, userOptions)
 	} else {
-		replaceFrom = obj;
-		options = extend(true, {}, defaultOptions, userOptions);
+		replaceFrom = obj
+		options = extend(true, {}, defaultOptions, userOptions)
 	}
 
-  var log = function (result, from, to, fileName) {
-    if (!options.logs.enabled || (!options.logs.notReplaced && !result)) return;
+	const log = function (result, from, to, fileName) {
+		if (typeof options.logs === 'object' && (!options.logs.enabled || (!options.logs.notReplaced && !result))) return
 
-    var _result = result ? 'Replaced' : 'Not Replaced';
+		gutil.log(
+			(result ? 'Replaced' : 'Not Replaced') + ' ' +
+			gutil.colors.cyan(from) +
+			(to ? (' to ' + gutil.colors.cyan(to)) : '') +
+			' in file: ' + gutil.colors.magenta(fileName)
+		)
 
-    gutil.log(_result + ' ' + gutil.colors.cyan(from) + (to ? (' to ' + gutil.colors.cyan(to)) : '') + ' in file: ' + gutil.colors.magenta(fileName));
+		return true
+	}
 
-    return true;
-  };
+	return through.obj(function (file, enc, callback) {
+		const fileName = file.path.split('/')[file.path.split('/').length - 1]
 
-  return through.obj(function (file, enc, callback) {
-    var fileName = file.path.split('/')[file.path.split('/').length - 1];
+		const _replaceTo = function (replacement) {
+			if (typeof replaceTo === 'function') {
+				const replaceFunctionResult = replaceTo.apply(replaceTo, arguments)
 
-    var _replaceTo = function (replacement) {
-      if (typeof replaceTo === 'function') {
-        var replaceFunctionResult = replaceTo.apply(replaceTo, arguments);
+				log(true, replacement, replaceFunctionResult, fileName)
 
-        log(true, replacement, replaceFunctionResult, fileName);
+				return replaceFunctionResult
+			}
 
-        return replaceFunctionResult;
-      }
+			log(true, replacement, replaceTo, fileName)
 
-      log(true, replacement, replaceTo, fileName);
+			return replaceTo
+		}
 
-      return replaceTo;
-    };
+		if (file.isStream()) {
+			file.contents = file.contents.pipe(rs(search, replacement))
+			return callback(null, file)
+		}
 
-    if (file.isStream()) {
-      file.contents = file.contents.pipe(rs(search, replacement));
-      return callback(null, file);
-    }
+		if (file.isBuffer()) {
+			try {
+				let contents = String(file.contents)
+				const regex = replaceFrom instanceof RegExp
+					? replaceFrom
+					: new RegExp(replaceFrom, 'g')
 
-    if (file.isBuffer()) {
-      try {
-        var contents = String(file.contents);
-        var regex = replaceFrom instanceof RegExp
-          ? replaceFrom
-          : new RegExp(replaceFrom, 'g');
+				if (regex.test(contents)) {
+					contents = contents.replace(regex, _replaceTo)
+				} else {
+					log(false, regex, false, fileName)
+				}
 
-        if (regex.test(contents)) {
-          contents = contents.replace(regex, _replaceTo);
-        } else {
-          log(false, regex, false, fileName);
-        }
+				file.contents = new Buffer(contents)
+			} catch (e) {
+				return callback(new gutil.PluginError('gulp-string-replace', e))
+			}
+		}
 
-        file.contents = new Buffer(contents);
-      } catch (e) {
-        return callback(new gutil.PluginError('gulp-string-replace', e));
-      }
-    }
-
-    callback(null, file);
-  });
-};
+		callback(null, file)
+	})
+}
